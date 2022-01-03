40aed9c45e762720723422c8915c2d9c8c5eea96
JB <jbtortoise@protonmail.com>
2022-01-03 12:40:17 -0800

    add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <div>Homepage</div>
+      <Link to="/collection">Collection page</Link>
     </div>
   );
 }
---
+++ src/routes/Collection.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Collection() {
+  return (
+    <div className="Collection">
+      <div>Mushrohm collection here</div>
+      <Link to="/">go home</Link>
+    </div>
+  );
+}
+
+export default Collection;
