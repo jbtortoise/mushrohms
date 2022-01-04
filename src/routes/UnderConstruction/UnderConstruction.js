4e33dcd6c2c2c980ea78e9b4c6f13aa9664e8f38
JB <jbtortoise@protonmail.com>
2022-01-03 16:17:05 -0800

    improve links

--- src/routes/UnderConstruction/UnderConstruction.js
+++ src/routes/UnderConstruction/UnderConstruction.js
@@ -12,19 +12,22 @@ function UnderConstruction() {
       <MushrohmsLogo className="mushrohms-logo" />
       <div className="social-container">
         <a href="https://discord.gg/QUtph52z">
-          <DiscordLogo className="social-link discord" />
+          <DiscordLogo className="social-logo discord" />
         </a>
         <a href="https://odyssey.olympusdao.finance/#/spores">
-          <OlympusLogo className="social-link olympus" />
+          <OlympusLogo className="social-logo olympus" />
         </a>
         <a href="https://twitter.com/Mushrohms">
-          <TwitterLogo className="social-link twitter" />
+          <TwitterLogo className="social-logo twitter" />
         </a>
         <a href="https://opensea.io/collection/mushrohms">
-          <OpenseaLogo className="social-link" />
+          <OpenseaLogo className="social-logo" />
         </a>
-        <a href="https://etherscan.io/address/0x133ba8f869f3ae35a5ca840ba20acfa31b0e2a61">
-          <EtherscanLogo className="social-link etherscan" />
+        <a
+          href="https://etherscan.io/address/0x133ba8f869f3ae35a5ca840ba20acfa31b0e2a61"
+          className="etherscan-link"
+        >
+          <EtherscanLogo className="social-logo" />
         </a>
       </div>
     </div>
