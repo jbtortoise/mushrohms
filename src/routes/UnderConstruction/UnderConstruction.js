302dd40ecb6520ad576bce9491af9018eb850ccd
JB <jbtortoise@protonmail.com>
2022-01-03 18:12:14 -0800

    change inline svgs to imgs

--- src/routes/UnderConstruction/UnderConstruction.js
+++ src/routes/UnderConstruction/UnderConstruction.js
@@ -1,33 +1,51 @@
 import "./UnderConstruction.css";
-import { ReactComponent as MushrohmsLogo } from "./MushrohmsLogo.svg";
-import { ReactComponent as TwitterLogo } from "./TwitterLogo.svg";
-import { ReactComponent as DiscordLogo } from "./DiscordLogo.svg";
-import { ReactComponent as OlympusLogo } from "./OlympusLogo.svg";
-import { ReactComponent as OpenseaLogo } from "./OpenseaLogo.svg";
-import { ReactComponent as EtherscanLogo } from "./EtherscanLogo.svg";
+import MushrohmsLogo from "./MushrohmsLogo.svg";
+import TwitterLogo from "./TwitterLogo.svg";
+import DiscordLogo from "./DiscordLogo.svg";
+import OlympusLogo from "./OlympusLogo.svg";
+import OpenseaLogo from "./OpenseaLogo.svg";
+import EtherscanLogo from "./EtherscanLogo.svg";
 
 function UnderConstruction() {
   return (
     <div className="container">
-      <MushrohmsLogo viewBox="0 0 325 325" className="mushrohms-logo" />
+      <img
+        src={MushrohmsLogo}
+        alt="Mushrohms logo"
+        className="mushrohms-logo"
+      />
+      {/* <MushrohmsLogo viewBox="0 0 325 325" className="mushrohms-logo" /> */}
       <div className="social-container">
         <a href="https://discord.gg/QUtph52z">
-          <DiscordLogo className="social-logo discord" />
+          <img
+            src={DiscordLogo}
+            alt="discord logo"
+            className="social-logo discord"
+          />
         </a>
         <a href="https://odyssey.olympusdao.finance/#/spores">
-          <OlympusLogo className="social-logo olympus" />
+          <img
+            src={OlympusLogo}
+            alt="olympus logo"
+            className="social-logo olympus"
+          />
         </a>
         <a href="https://twitter.com/Mushrohms">
-          <TwitterLogo className="social-logo twitter" />
+          <img
+            src={TwitterLogo}
+            alt="twitter logo"
+            className="social-logo twitter"
+          />
         </a>
         <a href="https://opensea.io/collection/mushrohms">
-          <OpenseaLogo className="social-logo" />
+          <img src={OpenseaLogo} alt="opensea logo" className="social-logo" />
         </a>
-        <a
-          href="https://etherscan.io/address/0x133ba8f869f3ae35a5ca840ba20acfa31b0e2a61"
-          className="etherscan-link"
-        >
-          <EtherscanLogo className="social-logo" />
+        <a href="https://etherscan.io/address/0x133ba8f869f3ae35a5ca840ba20acfa31b0e2a61">
+          <img
+            src={EtherscanLogo}
+            alt="etherscan logo"
+            className="social-logo"
+          />
         </a>
       </div>
     </div>
