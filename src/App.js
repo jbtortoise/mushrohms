039e1f0ab5d2fa308822d4efdf7bd1eb246a3e6c
JB <jbtortoise@protonmail.com>
2022-01-22 12:34:43 -0800

    first gen structure

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ function App() {
           alt="Mushrohm Kingdom"
         />
       </div>
-      <div className="welcome-container">
+
+      <section className="welcome-container">
         <div className="welcome-text-container">
           <div className="welcome-title">Welcome to the Mushrohm Kingdom</div>
           <p className="welcome-text-content">
@@ -31,7 +32,39 @@ function App() {
           />
           <div className="welcome-image-label">Glow Mushrohm</div>
         </div>
-      </div>
+      </section>
+
+      <section className="first-gen-container">
+        <div>1st Gen Mushrohms</div>
+        <p>
+          These 1,500 1st Generation Mushrohms are the premier collection of the
+          Mushrohm Kingdom on Ethereum Mainnet. These powerful Shrohms feature
+          many unique properties that qulify for a reproductive Sporedrop
+          mechanism, as well as allow membership access to the Shrohmie Council,
+          where members are able to contribute to the direction of the project,
+          help manage the Shrohmie Treasury, and receive access to exclusive
+          discord channels. Soon the 1st Gen Mushrohms will be able to join the
+          Treasure DAO Bridgeworld metaverse via a unique reversible NFT
+          Trasmutation Bridge from Eth Mainnet to Arbitrum Network.
+        </p>
+        <div className="first-gen-image-container">
+          <img
+            className="first-gen-image"
+            src="https://mushrohms.io/images/chanterelle-mushrohm.png"
+            alt="Chanterelle Mushrohm"
+          />
+          <img
+            className="first-gen-image"
+            src="https://mushrohms.io/images/pe-mushrohm.png"
+            alt="PE Mushrohm"
+          />
+          <img
+            className="first-gen-image"
+            src="https://mushrohms.io/images/cerberus-mushrohm.png"
+            alt="Cerberus Mushrohm"
+          />
+        </div>
+      </section>
       <div id="sporedrops">sporedrops</div>
     </div>
   );
