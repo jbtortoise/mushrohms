9f44efcd1417e0abcadc6efdeee6b36f49595f93
JB <jbtortoise@protonmail.com>
2022-01-23 16:36:02 -0800

    add landscape logo

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -4,7 +4,11 @@ import NavLinks from "../NavLinks/NavLinks";
 function TopNav() {
   return (
     <div className="top-nav-container">
-      <div>LOGO HERE</div>
+      <img
+        id="logo-landscape"
+        src="https://mushrohms.io/images/Logo_Landscape.png"
+        alt="Mushrohms Logo"
+      />
       <NavLinks />
       <div>social links</div>
     </div>
