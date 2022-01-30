6f834b2ea60a93df68359a7de482878d01032415
JB <jbtortoise@protonmail.com>
2022-01-30 13:28:42 -0800

    add fonts

--- src/components/MobileNav/MobileNav.js
+++ src/components/MobileNav/MobileNav.js
@@ -1,11 +1,15 @@
 import "./MobileNav.css";
-import NavLinks from "../NavLinks/NavLinks";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function MobileNav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
     <div className="mobile-nav-container">
-      <div className="hamburger-menu">
+      <div className="hamburger-menu" onClick={toggleMenu}>
         <i class="fas fa-lg fa-bars"></i>
       </div>
       <div className="mobile-nav-inner-container">
@@ -17,53 +21,10 @@ function MobileNav() {
           />
         </Link>
       </div>
+      {isMenuOpen ? (
+        <div className="mobile-menu-container">Mobile Menu Open!</div>
+      ) : null}
     </div>
-
-    // <div className="top-nav-container">
-
-    //   <div>
-    //     <a target="_blank" rel="noreferrer" href="https://discord.gg/QUtph52z">
-    //       <img
-    //         src="https://mushrohms.io/images/icon-discord.png"
-    //         alt="discord logo"
-    //         className="social-logo discord"
-    //       />
-    //     </a>
-    //     <a
-    //       target="_blank"
-    //       rel="noreferrer"
-    //       href="https://twitter.com/Mushrohms"
-    //     >
-    //       <img
-    //         src="https://mushrohms.io/images/icon-twitter.png"
-    //         alt="twitter logo"
-    //         className="social-logo twitter"
-    //       />
-    //     </a>
-    //     <a
-    //       target="_blank"
-    //       rel="noreferrer"
-    //       href="https://opensea.io/collection/mushrohms"
-    //     >
-    //       <img
-    //         src="https://mushrohms.io/images/icon-opensea.png"
-    //         alt="opensea logo"
-    //         className="social-logo"
-    //       />
-    //     </a>
-    //     <a
-    //       target="_blank"
-    //       rel="noreferrer"
-    //       href="https://etherscan.io/address/0x133ba8f869f3ae35a5ca840ba20acfa31b0e2a61"
-    //     >
-    //       <img
-    //         src="https://mushrohms.io/images/icon-etherscan.png"
-    //         alt="etherscan logo"
-    //         className="social-logo"
-    //       />
-    //     </a>
-    //   </div>
-    // </div>
   );
 }
 
