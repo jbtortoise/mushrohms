e34546767b0bf22ee5df00e1e75f7e6d7e334ec6
JB <jbtortoise@protonmail.com>
2022-02-06 09:38:09 -0800

    mobile styles, shrohmie council

--- src/ShrohmCouncil.js
+++ src/ShrohmCouncil.js
@@ -10,7 +10,27 @@ function ShromhCouncil() {
           alt="Mushrohm Kingdom"
         />
       </div>
-      <div className="page-content"></div>
+      <div className="page-content">
+        <p className="shrohm-council-p1">
+          Shrohmie Council gives voice to 1st Gen holders by allowing the
+          community to participate in the direction of Mushrohms. Once weekly on
+          Wednesdays at 7pm UTC the Shrohmie Council meets to discuss
+          initiatives, collaborations, alpha, proposals, and more. Mushrohms
+          currently manages a 6-figure Shrohmie Treasury to continue to fund and
+          grow the project. The Shrohmie community is the lifeblood of
+          Mushrohms, and so transparency and camaraderie are the main aims of
+          the Shrohmie Council. At this time, the Shrohmie Council is exclusive
+          to verified 1st Gen Mushrohm NFT holders.
+        </p>
+        <p>
+          Participation in the council, as well as other Mushrohm related
+          events, are rewarded with unique collectible Shrohmie Cap POAPs. The
+          non-transferable Shrohmie Caps are a way for Shrohmies to measure and
+          flex their Mushrohm related achievements, and plans are underway to
+          reward holders of a certain number of Shrohmie Caps with unique perks
+          and airdrops. Shrohm on frens!
+        </p>
+      </div>
     </div>
   );
 }
